827b374ae86440f4e5ed39cb7eb67f9665449612
imloadinqqq <imloadinq7579@gmail.com>
2024-11-20 19:58:37 -0600

    added loop to add more employees to database using user input

--- sqlpractice.py
+++ sqlpractice.py
@@ -4,10 +4,70 @@ import os
 
 load_dotenv()
 
-# user input
 fname = input("What is your first name? ")
 lname = input(f"Hi, {fname}! What's your last name? ")
 pos = input("Enter your desired position: ")
+
+resetQuery = """
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees(
+id int PRIMARY KEY AUTO_INCREMENT,
+FirstName varchar(255),
+LastName varchar(255),
+Position varchar(255)
+)
+"""
+migrateTable = """
+DROP TABLE IF EXISTS save;
+CREATE TABLE save(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Position varchar(255)
+)
+"""
+insertQuery = "INSERT INTO employees (FirstName, LastName, Position) VALUES (%s, %s, %s)"
+copyQuery = "INSERT INTO save SELECT * FROM employees"
+selectQuery = "SELECT * FROM employees"
+
+def insertNewEmployee():
+    while True:
+        fname = input("Enter first name: ")
+        lname = input("Enter last name: ")
+        pos = input("Enter position: ")
+
+        try:
+            connection = mysql.connector.connect(
+                user=os.getenv("DB_USER"), 
+                password=os.getenv("DB_PASS"),
+                host=os.getenv("DB_HOST"), 
+                database=os.getenv("DB_NAME")
+            )
+
+            if connection.is_connected():
+                print("CONNECTED TO DB")
+            
+            with connection.cursor() as cursor:
+                cursor.execute(insertQuery, (fname, lname, pos))
+                connection.commit()
+
+                cursor.execute(selectQuery)
+                results = cursor.fetchall()
+                print("Employees:")
+                for row in results:
+                    print(row)
+
+        except mysql.connector.Error as err:
+            print(f"Error Connecting: {err}")
+        finally:
+            if connection.is_connected():
+                connection.close()
+                print("CONNECTION CLOSED")
+
+        response = input("Would you like to insert another employee? (y/n): ").lower()
+        if response != 'y':
+            print("Exiting employee insertion.")
+            break
 
 try:
     # create connection
@@ -21,37 +81,12 @@ try:
     if connection.is_connected():
         print("CONNECTED TO DB")
 
-    # queries
-    resetQuery = """
-    DROP TABLE IF EXISTS employees;
-    CREATE TABLE employees(
-        id int PRIMARY KEY,
-        FirstName varchar(255),
-        LastName varchar(255),
-        Position varchar(255)
-    )
-    """
-    migrateTable = """
-    DROP TABLE IF EXISTS save;
-    CREATE TABLE save(
-        id int PRIMARY KEY,
-        FirstName varchar(255),
-        LastName varchar(255),
-        Position varchar(255)
-    )
-    """
-    insertQuery = "INSERT INTO employees (id, FirstName, LastName, Position) VALUES (%s, %s, %s, %s)"
-    copyQuery = "INSERT INTO save SELECT * FROM employees"
-    selectQuery = "SELECT * FROM save"
-
-    # cursor executions
     with connection.cursor() as cursor:
         for statement in resetQuery.split(';'):
             if statement.strip():
                 cursor.execute(statement.strip())
 
-        cursor.execute(insertQuery, (1, fname, lname, pos))
-
+        cursor.execute(insertQuery, (fname, lname, pos))
         connection.commit()
 
         cursor.execute(selectQuery)
@@ -67,10 +102,12 @@ try:
         cursor.execute(copyQuery)
         connection.commit()
         print("DATA SUCCESSFULLY COPIED TO 'save'")
+
 except mysql.connector.Error as err:
     print(f"Error Connecting: {err}")
-    # close connection
 finally:
     if connection.is_connected():
         connection.close()
         print("CONNECTION CLOSED")
+
+insertNewEmployee()
